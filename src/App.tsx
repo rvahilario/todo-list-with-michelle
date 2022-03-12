1957f4bd8b561f5e3487a84116805f07db0e2d50
Renan Hilario <rvahilario@gmail.com>
2022-03-12 18:18:05 -0300

    ✨ add list itens

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
 
 const App = () => {
+	const [list, setList] = useState<Item[]>([
+		{ id: 0, name: 'Shopping', done: false },
+		{ id: 1, name: 'Buy bread at market', done: true },
+	]);
+
 	return (
 		<C.Container>
 			<C.Area>
